abc6ba94adfa20023f6ae3e6b965391ec45a47ea
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-10-16 19:01:17 -0400

    WIP

--- src/uhinet/data/landsat.py
+++ src/uhinet/data/landsat.py
@@ -15,7 +15,14 @@ elif args.log_level == 'DEBUG':
     logging.root.setLevel(logging.DEBUG)
 
 if __name__ == "__main__":
+    # task = ee.batch.Export.image.toDrive(
+    #         image=myImg,
+    #         region=myRegion.getInfo()['coordinates'],
+    #         description='myDescription',
+    #         folder='myGDriveFolder',
+    #         fileNamePrefix='myFilePrefix',
+    #         scale=myScale,
+    #         crs=myCRS)
+    # task.start()
+    # task.status()
     ee.Initialize()
-    img = ee.Image('LANDSAT/LT05/C01/T1_SR/LT05_034033_20000913')
-    print(img)
-    print(img.getInfo())
