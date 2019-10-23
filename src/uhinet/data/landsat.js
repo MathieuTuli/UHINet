8d34a9bd96bbffa7cce6d974f6d4001f37c14041
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-10-23 19:46:33 -0400

    WIP

--- src/uhinet/data/landsat.js
+++ src/uhinet/data/landsat.js
@@ -1,8 +1,5 @@
-//var geometry = ee.FeatureCollection('ft:1Fvl6dFuSgoX9phkzLinP8O6lz9GmsvsDiNJsSvro').geometry();
-// var geometry = tinh.filter(ee.Filter.eq("Name", "Can Tho City"));
-var geometry = ee.Geometry.Rectangle([43.945970, -79.477810,
-                                      43.654350, -79.407530])
 //cloud mask
+var geometry = ee.Geometry.Rectangle([-79.49947,43.607519,-79.269364,43.71388])
 function maskL8sr(image) {
   // Bits 3 and 5 are cloud shadow and cloud, respectively.
   var cloudShadowBitMask = (1 << 3);
@@ -33,14 +30,14 @@ var vizParams2 = {
 {
 var col = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
 .map(maskL8sr)
-.filterDate('2018-01-01','2018-12-30')
+.filterDate('2018-01-01','2018-12-31')
 .filterBounds(geometry);
 }
 print(col, 'coleccion');
 
 //median
 {
-var image = col.median().clip(geometry);
+var image = col.median();
 print(image, 'image');
 Map.addLayer(image, vizParams2);
 }
@@ -56,28 +53,11 @@ Map.addLayer(ndvi, ndviParams, 'ndvi');
 //
 
 //select thermal band 10(with brightness tempereature), no BT calculation 
- var thermal= image.select('B10').multiply(0.1);
+var thermal= image.select('B10').multiply(0.1);
+var b10Params = {min: 2878000, max: 3046000, palette: ['blue', 'white', 'green']};
+Map.addLayer(thermal, b10Params, 'thermal');
  
- /*{
-var min = ee.Number(thermal.reduceRegion({
-   reducer: ee.Reducer.min(),
-   geometry: geometry,
-   scale: 30,
-   maxPixels: 1e9
-   }).values().get(0));
-print(min, 'min');
-var max = ee.Number(thermal.reduceRegion({
-    reducer: ee.Reducer.max(),
-   geometry: geometry,
-   scale: 30,
-   maxPixels: 1e9
-   }).values().get(0));
-print(max, 'max')
-}*/
- var b10Params = {min: 2878000, max: 3046000, palette: ['blue', 'white', 'green']};
- Map.addLayer(thermal, b10Params, 'thermal');
-
-
+ 
 // find the min and max of NDVI
 {
 var min = ee.Number(ndvi.reduceRegion({
@@ -100,7 +80,7 @@ print(max, 'max')
 {
 var fv = ndvi.subtract(min).divide(max.subtract(min)).rename('FV'); 
 print(fv, 'fv');
-//Map.addLayer(fv);
+Map.addLayer(fv);
 }
 
 /////////////
@@ -111,26 +91,11 @@ print(fv, 'fv');
   var a= ee.Number(0.004);
   var b= ee.Number(0.986);
   var EM=fv.multiply(a).add(b).rename('EMM');
-  
-  /*{
-var min = ee.Number(EM.reduceRegion({
-   reducer: ee.Reducer.min(),
-   geometry: geometry,
-   scale: 30,
-   maxPixels: 1e9
-   }).values().get(0));
-print(min, 'min');
-var max = ee.Number(EM.reduceRegion({
-    reducer: ee.Reducer.max(),
-   geometry: geometry,
-   scale: 30,
-   maxPixels: 1e9
-   }).values().get(0));
-print(max, 'max')
-}*/
   var imageVisParam2 = {min: 0.98, max: 0.99, palette: ['blue', 'white', 'green']};
   Map.addLayer(EM, imageVisParam2,'EMM');
 
+
+  //LST c,d,f, p1, p2, p3 are assigned variables to write equaton easily
 var LST = thermal.expression(
     '(Tb/(1 + (0.001145* (Tb / 1.438))*log(Ep)))-273.15', {
       'Tb': thermal.select('B10'),
@@ -171,6 +136,10 @@ Map.addLayer(LST, {min: 15, max: 32, palette: ['white', 'red']},'LST');
 
   
   
-  
-  
-  
+
+
+
+
+
+
+
