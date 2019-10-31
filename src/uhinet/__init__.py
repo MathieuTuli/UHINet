4a755f3d65ce61b56ecf2282be1a825be1b56cad
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-10-31 19:47:02 -0400

    Update __init__.pys. Nothing special

---
+++ src/uhinet/__init__.py
@@ -0,0 +1,6 @@
+from argparse import ArgumentParser
+
+from .data.SentinelHubAccessor import SentinelHubAccessor
+
+if __name__ == "__main__":
+    print("uhinet")
---
+++ src/uhinet/data/__init__.py
@@ -0,0 +1,6 @@
+from argparse import ArgumentParser
+
+from .SentinelHubAccessor import SentinelHubAccessor
+
+if __name__ == "__main__":
+    print("uhinet.data")
