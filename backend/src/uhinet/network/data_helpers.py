c5ed1b041c8e8d3c6196efe071968f8d0478e35f
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-11-29 11:32:08 -0500

    Start refactoring pix2pix code for shell use

---
+++ backend/src/uhinet/network/data_helpers.py
@@ -0,0 +1,139 @@
+'''
+UHINET Network Pix2Pix Data Helpers
+
+@credit: Google (https://www.tensorflow.org/tutorials/generative/pix2pix)
+'''
+from typing import Tuple
+import tensorflow as tf
+
+from .components import ImageDirection
+
+
+# TODO : which direction , this used left to right
+def load(image_path: str,
+         direction: ImageDirection) -> Tuple[tf.Tensor, tf.Tensor]:
+    '''
+    @returns: Tuple, (input_image, target_image)
+    '''
+    image = tf.io.read_file(image_path)
+    image = tf.image.decode_jpeg(image)
+
+    w = tf.shape(image)[1]
+
+    w = w // 2
+    real_image = image[:, :w, :]
+    input_image = image[:, w:, :]
+
+    input_image = tf.cast(input_image, tf.float32)
+    target_image = tf.cast(real_image, tf.float32)
+
+    if direction == ImageDirection.AtoB:
+        return input_image, target_image
+    else:
+        return target_image, input_image
+
+
+def resize(input_image: tf.Tensor,
+           real_image: tf.Tensor,
+           height: int, width: int) -> Tuple[tf.Tensor, tf.Tensor]:
+    input_image = tf.image.resize(
+        input_image, [height, width],
+        method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    real_image = tf.image.resize(
+        real_image, [height, width],
+        method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    return input_image, real_image
+
+
+def random_crop(input_image: tf.Tensor,
+                real_image: tf.Tensor,
+                height: int,
+                width: int) -> Tuple[tf.Tensor, tf.Tensor]:
+    stacked_image = tf.stack([input_image, real_image], axis=0)
+    cropped_image = tf.image.random_crop(
+        stacked_image, size=[2, height, width, 3])
+
+    return cropped_image[0], cropped_image[1]
+
+
+def normalize(input_image, real_image):
+    input_image = (input_image / 127.5) - 1
+    real_image = (real_image / 127.5) - 1
+
+    return input_image, real_image
+
+
+@tf.function()
+def random_jitter(input_image, real_image):
+    # resizing to 286 x 286 x 3
+    input_image, real_image = resize(input_image, real_image, 286, 286)
+
+    # randomly cropping to 256 x 256 x 3
+    input_image, real_image = random_crop(input_image, real_image)
+
+    if tf.random.uniform(()) > 0.5:
+        # random mirroring
+        input_image = tf.image.flip_left_right(input_image)
+        real_image = tf.image.flip_left_right(real_image)
+
+    return input_image, real_image
+
+    # As you can see in the images below
+# that they are going through random jittering
+# Random jittering as described in the paper is to
+# 1. Resize an image to bigger height and width
+# 2. Randomnly crop to the original size
+# 3. Randomnly flip the image horizontally
+
+
+def load_image_train(image_file):
+    input_image, real_image = load(image_file)
+    input_image, real_image = random_jitter(input_image, real_image)
+    input_image, real_image = normalize(input_image, real_image)
+
+    return input_image, real_image
+
+
+def load_image_test(image_file):
+    input_image, real_image = load(image_file)
+    input_image, real_image = resize(input_image, real_image,
+                                     IMG_HEIGHT, IMG_WIDTH)
+    input_image, real_image = normalize(input_image, real_image)
+
+    return input_image, real_image
+
+
+def downsample(filters, size, apply_batchnorm=True):
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    result = tf.keras.Sequential()
+    result.add(
+        tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
+                               kernel_initializer=initializer, use_bias=False))
+
+    if apply_batchnorm:
+        result.add(tf.keras.layers.BatchNormalization())
+
+    result.add(tf.keras.layers.LeakyReLU())
+
+    return result
+
+
+def upsample(filters, size, apply_dropout=False):
+    initializer = tf.random_normal_initializer(0., 0.02)
+
+    result = tf.keras.Sequential()
+    result.add(
+        tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
+                                        padding='same',
+                                        kernel_initializer=initializer,
+                                        use_bias=False))
+
+    result.add(tf.keras.layers.BatchNormalization())
+
+    if apply_dropout:
+        result.add(tf.keras.layers.Dropout(0.5))
+
+    result.add(tf.keras.layers.ReLU())
+
+ 
--- backend/src/uhinet/network/pix2pix.py
+++ backend/src/uhinet/network/pix2pix.py
@@ -1,171 +1,33 @@
 '''
 UHINET Network Pix2Pix Module
 
-@author: Google (https://www.tensorflow.org/tutorials/generative/pix2pix)
+@credit: Google (https://www.tensorflow.org/tutorials/generative/pix2pix)
 '''
 from __future__ import absolute_import, division, print_function, \
     unicode_literals
-
-import tensorflow as tf
+from IPython.display import clear_output
+from typing import Tuple
 from pathlib import Path
 
-import os
-import time
 import matplotlib.pyplot as plt
-from IPython.display import clear_output
-PATH = os.path.join('/home/mat/github/U-of-T/capstone/uhinet/src/uhinet/data/images/', 'formatted/')
+import tensorflow as tf
+import time
+import os
+
+
+PATH = os.path.join('images')
 BUFFER_SIZE = 400
 BATCH_SIZE = 1
 IMG_WIDTH = 512
 IMG_HEIGHT = 512
 
 
-# TODO : which direction , this used left to right
-def load(image_file):
-    image = tf.io.read_file(image_file)
-    image = tf.image.decode_jpeg(image)
+class Pix2Pix():
+    def __init__(self) -> None:
+        ...
 
-    w = tf.shape(image)[1]
-
-    w = w // 2
-    real_image = image[:, :w, :]
-    input_image = image[:, w:, :]
-
-    input_image = tf.cast(input_image, tf.float32)
-    real_image = tf.cast(real_image, tf.float32)
-
-    return input_image, real_image
-
-inp, re = load(PATH+'train/0.jpg')
-
-def resize(input_image, real_image, height, width):
-    input_image = tf.image.resize(
-        input_image, [height, width],
-        method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-    real_image = tf.image.resize(
-        real_image, [height, width],
-        method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
-
-    return input_image, real_image
-
-
-def random_crop(input_image, real_image):
-    stacked_image = tf.stack([input_image, real_image], axis=0)
-    cropped_image = tf.image.random_crop(
-        stacked_image, size=[2, IMG_HEIGHT, IMG_WIDTH, 3])
-
-    return cropped_image[0], cropped_image[1]\
-
-# normalizing the images to [-1, 1]
-
-
-def normalize(input_image, real_image):
-    input_image = (input_image / 127.5) - 1
-    real_image = (real_image / 127.5) - 1
-
-    return input_image, real_image
-
-
-@tf.function()
-def random_jitter(input_image, real_image):
-    # resizing to 286 x 286 x 3
-    input_image, real_image = resize(input_image, real_image, 286, 286)
-
-    # randomly cropping to 256 x 256 x 3
-    input_image, real_image = random_crop(input_image, real_image)
-
-    if tf.random.uniform(()) > 0.5:
-        # random mirroring
-        input_image = tf.image.flip_left_right(input_image)
-        real_image = tf.image.flip_left_right(real_image)
-
-    return input_image, real_image
-
-    # As you can see in the images below
-# that they are going through random jittering
-# Random jittering as described in the paper is to
-# 1. Resize an image to bigger height and width
-# 2. Randomnly crop to the original size
-# 3. Randomnly flip the image horizontally
-
-
-def load_image_train(image_file):
-    input_image, real_image = load(image_file)
-    input_image, real_image = random_jitter(input_image, real_image)
-    input_image, real_image = normalize(input_image, real_image)
-
-    return input_image, real_image
-
-
-def load_image_test(image_file):
-    input_image, real_image = load(image_file)
-    input_image, real_image = resize(input_image, real_image,
-                                     IMG_HEIGHT, IMG_WIDTH)
-    input_image, real_image = normalize(input_image, real_image)
-
-    return input_image, real_image
-
-
-train_dataset = tf.data.Dataset.list_files(PATH+'train/*.jpg')
-
-
-train_dataset = train_dataset.map(
-    load_image_train,
-    num_parallel_calls=tf.data.experimental.AUTOTUNE)
-train_dataset = train_dataset.cache().shuffle(BUFFER_SIZE)
-train_dataset = train_dataset.batch(1)
-
-test_dataset = tf.data.Dataset.list_files(PATH+'test/*.jpg')
-test_dataset = test_dataset.map(load_image_test)
-test_dataset = test_dataset.batch(1)
-
-OUTPUT_CHANNELS = 3
-
-
-def downsample(filters, size, apply_batchnorm=True):
-    initializer = tf.random_normal_initializer(0., 0.02)
-
-    result = tf.keras.Sequential()
-    result.add(
-        tf.keras.layers.Conv2D(filters, size, strides=2, padding='same',
-                               kernel_initializer=initializer, use_bias=False))
-
-    if apply_batchnorm:
-        result.add(tf.keras.layers.BatchNormalization())
-
-    result.add(tf.keras.layers.LeakyReLU())
-
-    return result
-
-
-down_model = downsample(3, 4)
-down_result = down_model(tf.expand_dims(inp, 0))
-print(down_result.shape)
-
-
-def upsample(filters, size, apply_dropout=False):
-    initializer = tf.random_normal_initializer(0., 0.02)
-
-    result = tf.keras.Sequential()
-    result.add(
-        tf.keras.layers.Conv2DTranspose(filters, size, strides=2,
-                                        padding='same',
-                                        kernel_initializer=initializer,
-                                        use_bias=False))
-
-    result.add(tf.keras.layers.BatchNormalization())
-
-    if apply_dropout:
-        result.add(tf.keras.layers.Dropout(0.5))
-
-    result.add(tf.keras.layers.ReLU())
-
-    return result
-
-
-up_model = upsample(3, 4)
-up_result = up_model(down_result)
-print(up_result.shape)
+    def __str__(self) -> str:
+        return 'Pix2Pix'
 
 
 def Generator():
