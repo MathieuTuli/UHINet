25c06bf50059d572e9f224c6bd212462efc28744
licandow <quanzhou.li@mail.utoronto.ca>
2019-12-04 23:55:18 -0500

    change render file

--- frontend/run_map.py
+++ frontend/run_map.py
@@ -7,7 +7,7 @@ def map():
     s = f.read().splitlines()[0]
     f.close()
     key = 'https://maps.googleapis.com/maps/api/js?key='+ s +'&libraries=drawing&callback=initMap'
-    return render_template('map.html', key=key)
+    return render_template('map_single_polygon.html', key=key)
 
 if __name__ == '__main__':
     app.run(debug=True)
