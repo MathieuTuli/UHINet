e69858bce9b9938858e7659cd23f93e5c80b78cc
Mathieu Tuli <tuli.mathieu@gmail.com>
2019-12-05 13:01:35 -0500

    Syntax fixing

--- core/src/uhinet/frontend/run.py
+++ core/src/uhinet/frontend/run.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET"])
 def map():
     f = open("input", "r")
     s = f.read().splitlines()[0]
     f.close()
-    key = 'https://maps.googleapis.com/maps/api/js?key='+ s +'&libraries=drawing&callback=initMap'
+    key = 'https://maps.googleapis.com/maps/api/js?key=' + \
+        s + '&libraries=drawing&callback=initMap'
     return render_template('map_single_polygon.html', key=key)
+
 
 @app.route("/send_coordinates")
 def add_numbers():
@@ -17,6 +20,7 @@ def add_numbers():
     print('\n')
     image_name = str('image.png')
     return jsonify(image_name)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
