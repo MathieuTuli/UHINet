50135244859cdd266f660639efb6d03192c8d321
Mathieu Tuli <tuli.mathieu@gmail.com>
2020-01-14 12:28:34 -0500

    Adding Requests class for frontend to use

--- core/src/uhinet/backend/requests.py
+++ core/src/uhinet/backend/requests.py
@@ -1,17 +1,20 @@
 from typing import Tuple
+from pathlib import Path
 
 from TFPix2Pix import Predictor
 from ..frontend.components import Layer, Polygon, Orientation, Season
-from .data.image_formatting import alter_area
 from .data.sentinel_hub import SentinelHubAccessor
+from .data.image_formatting import alter_area
 
 
 class Requests():
-    def __init__(self):
+    def __init__(self,
+                 instance_id: Path,
+                 weights_file: Path) -> None:
         self.predictor = Predictor()
-        self.sha = SentinelHubAccessor()
+        self.accessor = SentinelHubAccessor()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return 'Requests'
 
     def get_raw_LST() -> Layer:
